c549d6dd7cd7d1f7308187f95ea653d4fc6024b3
Jiyoosh <165247825+Jiyoosh@users.noreply.github.com>
2025-02-13 15:11:54 +0000

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+function buttonalert(){
+  
+    alert("ACCOUNT CREATED SUCCESSFULLY!");
+  }
+  function  buttonalert2(){
+    alert("You message have been delivered!");
+  }
+  
+  function  buttonalert3(){
+    alert("Call has been requested!");
+  }
+  
